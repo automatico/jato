a2c3e60792eb5ef71da7a13e5acd27260d6684b1
Brad Searle <bradleysearle@gmail.com>
2021-03-16 20:20:32 +1000

    looks good enough for now

--- pkg/jato/telnet.go
+++ pkg/jato/telnet.go
@@ -16,12 +16,12 @@ func Telnet(jt Jato) Results {
 	chanResult := make(chan Result)
 	for _, dev := range jt.Devices.Devices {
 		go func(d Device, c CommandExpect) {
-			chanResult <- runnerTelnet(d, c)
+			chanResult <- telnetRunner(d, c)
 		}(dev, commands)
 	}
 
 	for range jt.Devices.Devices {
-		timeout := time.After(5 * time.Second)
+		timeout := time.After(6 * time.Second)
 		select {
 		case res := <-chanResult:
 			results.Results = append(results.Results, res)
@@ -33,11 +33,13 @@ func Telnet(jt Jato) Results {
 	return results
 }
 
-func runnerTelnet(dev Device, commands CommandExpect) Result {
+func telnetRunner(dev Device, commands CommandExpect) Result {
 	timeNow := time.Now().Unix()
 	r := Result{}
 	r.Device = dev.Name
+	r.Timestamp = timeNow
 
+	// fmt.Println("DIALING")
 	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", dev.IP, telnetPort))
 	if err != nil {
 		fmt.Println("dial error:", err)
@@ -45,6 +47,8 @@ func runnerTelnet(dev Device, commands CommandExpect) Result {
 		return r
 	}
 	defer conn.Close()
+	// fmt.Println("FINISHED DIALING")
+
 	auth(conn)
 
 	for _, cmd := range commands.CommandExpect {
@@ -52,20 +56,29 @@ func runnerTelnet(dev Device, commands CommandExpect) Result {
 		r.CommandOutputs = append(r.CommandOutputs, CommandOutput{Command: cmd.Command, Output: res})
 		if err != nil {
 			fmt.Println(res)
-			// fmt.Println(err)
+			fmt.Println(err)
 		}
 	}
-	r.OK = true
-	r.Timestamp = timeNow
+
+	theTimeNow := time.Now().Unix()
+	// fmt.Println(theTimeNow)
+	// fmt.Println(timeNow + 5)
+	if theTimeNow > timeNow+5 {
+		// Consider the device timed out sending commands
+		fmt.Println("Timeout waiting for commands")
+		r.OK = false
+	} else {
+		r.OK = true
+	}
 	return r
 }
 
 func auth(conn net.Conn) {
 	commands := CommandExpect{
 		[]Expect{
-			{Command: "", Expecting: "Username:", Timeout: 5},
-			{Command: "admin", Expecting: "Password:", Timeout: 5},
-			{Command: "Juniper", Expecting: "#", Timeout: 5},
+			{Command: "", Expecting: "Username:", Timeout: 2},
+			{Command: "admin", Expecting: "Password:", Timeout: 2},
+			{Command: "Juniper", Expecting: "#", Timeout: 2},
 		},
 	}
 	for _, cmd := range commands.CommandExpect {
